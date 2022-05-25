da90534d167cda4fd178727d9be7320f312daac4
Borislava <bbardarska@gmail.com>
2022-05-25 16:52:00 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//declaration
+@Component({
+  //html tag to embed our component
+  selector: 'app-root',
+  //the html file we are using
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//we put properties and methods here
+export class AppComponent {}
